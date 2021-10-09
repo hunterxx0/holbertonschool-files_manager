3c39797dee4fef0f5c61751503dbf3920b96ea6c
hunterxx0 <azouzmhamed@gmail.com>
2021-10-09 16:19:46 +0100

    first

---
+++ utils/db.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+	constructor(host='localhost', port=27017, database='files_manager') {
+		MongoClient.connect(`mongodb://${host}:${port}`, {useUnifiedTopology: true}, (err, client) => {
+			if (err) throw err;
+			this.connect = client.isConnected();
+			this.db = client.db(database);
+		});		
+	}
+
+	isAlive() {
+		if (this.db) return true;
+		return false;
+	}
+
+	async nbUsers() {
+		return this.db.collection('users').countDocuments()
+	}
+
+	async nbFiles() {
+		return this.db.collection('files').countDocuments()
+	}
+
+}
+
+const dbClient = new DBClient(
+	process.env.DB_HOST,
+	process.env.DB_PORT,
+	process.env.DB_DATABASE);
+module.exports = dbClient;
---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+	constructor() {
+		this.client = createClient();
+		this.connect = true;
+		this.client.on('error', (err) => {
+			this.connect = false;
+			console.log(err);
+		});
+		this.asyn = promisify(this.client.get).bind(this.client);
+	}
+
+	isAlive() {
+		return this.connect;
+	}
+
+	async get(key) {
+		return this.asyn(key);
+	}
+
+	async set(key, value, lmt) {
+		this.client.set(key, value)
+		this.client.expire(key, lmt);
+	}
+
+	async del(key) {
+		this.client.del(key)
+	}
+
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
