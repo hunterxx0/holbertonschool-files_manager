79c76e30ac0e311994feebfc83cdfafaa5a31fa5
hunterxx0 <azouzmhamed@gmail.com>
2021-10-13 14:50:22 +0100

    first

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require('./routes/index');
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const app = express();
 
