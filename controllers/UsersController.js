59e0a7830a3203f6150a535f8d2872607c459f2e
hunterxx0 <azouzmhamed@gmail.com>
2021-10-13 16:33:33 +0100

    first

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,21 +1,21 @@
-const dbClient = require('../utils/db');
 const crypto = require('crypto');
+const dbClient = require('../utils/db');
 
 class UsersController {
   static async postNew(request, response) {
-    const { email, password } =  request.body;
+    const { email, password } = request.body;
 
     if (!email) return response.status(400).send({ error: 'Missing email' });
     if (!password) return response.status(400).send({ error: 'Missing password' });
-    const result = await dbClient.db.collection('users').findOne({email});
-    if ( result?.email === email ) {
-    	return response.status(400).send({ error: 'Already exist' });
+    const result = await dbClient.db.collection('users').findOne({ email });
+    if (result && result.email === email) {
+      return response.status(400).send({ error: 'Already exist' });
     }
     const shasum = crypto.createHash('sha1');
     shasum.update(password);
-  	const pass = shasum.digest('hex');
+    const pass = shasum.digest('hex');
   	let _id;
-  	await dbClient.db.collection('users').insertOne({email, password: pass}, (err, res) => {
+  	await dbClient.db.collection('users').insertOne({ email, password: pass }, (err, res) => {
   		_id = res.insertedId;
   	});
 
