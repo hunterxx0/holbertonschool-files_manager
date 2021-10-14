6ef6ac600fe5df2d9b2f17885e3cf8b361b5d655
yacinekedidi <kedidiyacine@gmail.com>
2021-10-14 14:57:35 +0100

    f

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,10 @@
 const sha1 = require('sha1');
 import decode from 'decode-base64';
+import { report } from 'superagent';
 const { v4 } = require('uuid');
 const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
+const { ObjectId } = require('mongodb');
 
 class AuthController {
   static async getConnect(request, response) {
@@ -25,7 +27,21 @@ class AuthController {
   }
 
   static async getDisconnect(request, response) {
-    const { email, password } = request.body;
+    const { 'x-token': xToken } = request.headers;
+    const userId = await redisClient.get(`auth_${xToken}`);
+    if (!userId) return response.status(401).send({ error: 'Unauthorized' });
+    await redisClient.del(`auth_${xToken}`);
+    return response.status(204).send('');
+  }
+
+  static async getMe(request, response) {
+    const { 'x-token': xToken } = request.headers;
+    const id = await redisClient.get(`auth_${xToken}`);
+    const result = await dbClient.db
+      .collection('users')
+      .findOne({ _id: ObjectId(id) });
+    if (!result) return response.status(401).send({ error: 'Unauthorized' });
+    return response.status(200).send({ id, email: result.email });
   }
 }
 
