a4ab36c78ba619b7516685f76f2f8d6a88db2d61
yacinekedidi <kedidiyacine@gmail.com>
2021-10-15 15:05:11 +0100

    f

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -95,7 +95,7 @@ class FilesController {
     const { id } = request.params;
     const file = await dbClient.db
       .collection('files')
-      .findOne({ _id: ObjectId(id), userId });
+      .findOne({ _id: ObjectId(id) });
     if (!file) return response.status(404).send({ error: 'Not found' });
 
     const { _id, userId: uid, name, type, isPublic, parentId } = file;
